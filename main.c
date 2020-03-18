9ac20aa9471ed02a982b4a23930bcc8d8f6e4d17
hurtadojara <1616@holbertonschool.com>
2020-03-18 13:39:05 -0500

    main from qa REVIEW search for errors

--- main.c
+++ main.c
@@ -1,57 +1,71 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <limits.h>
+#include <stdio.h>
 #include "holberton.h"
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	int alen, elen;
-	char c = 'z';
-	char *str = NULL;
-	printf("START OF TEST\n");
-	printf("=====================\n");
-	printf("printf(NULL)  : %d\n", printf(str));
-	printf("_printf(NULL) : %d\n", _printf(str));
-	printf("printf(\"%\") : %d\n", printf("%"));
-	printf("_printf(\"%\"): %d\n", _printf("%"));
-	str = "hello, world";
-	printf("=====================\n");
-	printf("*****CHAR*****\n");
-	printf("=====================\n");
-	printf("Expected   : %c\n", 'a');
-	_printf("Actual     : %c\n", 'a');
-	elen = printf("Expected   : %c\n", c);
-	alen = _printf("Actual     : %c\n", c);
-	printf("Expected   : %i\n", elen);
-	printf("Actual     : %i\n", alen);
-	printf("Expected   : %cc\n", 'a');
-	_printf("Actual     : %cc\n", 'a');
-	printf("Expected   : %c\n", 53);
-	_printf("Actual     : %c\n", 53);
-	printf("Expected   : %c.\n", '\0');
-	_printf("Actual     : %c.\n", '\0');
-	printf("Expected   : %%%c\n", 'y');
-	_printf("Actual     : %%%c\n", 'y');
-	printf("=====================\n");
-	printf("*****STRINGS*****\n");
-	printf("=====================\n");
-	alen = elen = 0;
-	printf("Expected   : %s\n", "holberton");
-	_printf("Actual     : %s\n", "holberton");
-	printf("Expected   : %s$\n", "");
-	_printf("Actual     : %s$\n", "");
-	elen = printf("Expected   : %s\n", str);
-	alen = _printf("Actual     : %s\n", str);
-	elen = printf("%s", "");
-	alen = _printf("%s", "");
-	printf("Expected   : %i\n", elen);
-	printf("Actual     : %i\n", alen);
-	printf("Expected   : %sschool\n", "holberton");
-	_printf("Actual     : %sschool\n", "holberton");
-	elen = printf("Expected: %c%%%c%%%%%s%c%s%s\n", INT_MAX, INT_MIN, "hello", 0, "holberton", 0);
-	alen = _printf("Actual  : %c%%%c%%%%%s%c%s%s\n", INT_MAX, INT_MIN, "hello", 0, "holberton", 0);
-	printf("Expected: %d\n", elen);
-	printf("Actual  : %d\n", alen);
-	printf("Expected  %i %d\n", 0032, 0032);
-        _printf("Actual %i %d \n", 0032, 0032);
+  char str[] = "hijodetuputamadrecheckerdemierdametieneshastalaputamadreconesteputoproyectodesugranputisimamadreesteesunstringlargoynsequemasescribirAIUDAAAAAAAAAAAAAAAAAA";
+
+_printf("Let's print a simple sentence.\n");
+printf("Let's print a simple sentence.\n");
+
+_printf("%c\n", 'S');
+printf("%c\n", 'S');
+
+_printf("A char inside a sentence: %c. Did it work?\n", 'F');
+printf("A char inside a sentence: %c. Did it work?\n", 'F');
+
+_printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+
+_printf("%s", "This sentence is retrieved from va_args!\n");
+printf("%s", "This sentence is retrieved from va_args!\n");
+
+_printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+
+_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0); //verificar si hay error aca
+printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0); //verificar si hay error aca
+
+_printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+
+_printf("%%\n");
+printf("%%\n");
+
+_printf("Should print a single percent sign: %%\n");
+printf("Should print a single percent sign: %%\n");
+
+_printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+
+_printf("css%ccs%scscscs\n", 'T', "Test");
+printf("css%ccs%scscscs\n", 'T', "Test");
+
+_printf("str\n"); //string grande
+printf("str\n"); //string grande
+
+_printf("man gcc:\n%s", str);
+printf("man gcc:\n%s", str);
+
+_printf("%c\n", '\0');
+printf("%c\n", '\0');
+
+_printf(NULL);
+printf(NULL);
+
+_printf("%\n");
+printf("%\n");
+
+_printf("%!\n");
+printf("%!\n");
+
+_printf("%K\n");
+printf("%K\n");
+
 }
