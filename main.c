181466bc93ce2e5f8ea3420a316e02f3a446ebd9
hurtadojara <1616@holbertonschool.com>
2020-03-18 20:29:50 -0500

    ready

--- main.c
+++ main.c
@@ -9,6 +9,8 @@
  */
 int main(void)
 {
+  long int l = INT_MAX + 1024;
+  
 printf("%d", 1024);
 _printf("%d", 1024);
 
@@ -24,11 +26,11 @@ _printf("%d", 0);
   printf("%d", INT_MIN);
   _printf("%d", INT_MIN);
 
-  printf("%d", l); where l is a long int equals to INT_MAX + 1024
-  _printf("%d", l); where l is a long int equals to INT_MAX + 1024
+  printf("%d", l); //where l is a long int equals to INT_MAX + 1024
+  _printf("%d", l); //where l is a long int equals to INT_MAX + 1024
 
-  printf("%d", l); where l is a long int equals to INT_MIN - 1024
-  _printf("%d", l); where l is a long int equals to INT_MIN - 1024
+  printf("%d", l); //where l is a long int equals to INT_MIN - 1024
+  _printf("%d", l); //where l is a long int equals to INT_MIN - 1024
 
   printf("There is %d bytes in %d KB\n", 1024, 1);
   _printf("There is %d bytes in %d KB\n", 1024, 1);
@@ -54,11 +56,11 @@ _printf("%d", 0);
   printf("%i", INT_MIN);
   _printf("%i", INT_MIN);
 
-  printf("%i", l); where l is a long int equals to INT_MAX + 1024
-  _printf("%i", l); where l is a long int equals to INT_MAX + 1024
+  printf("%i", l); //where l is a long int equals to INT_MAX + 1024
+  _printf("%i", l); //where l is a long int equals to INT_MAX + 1024
 
-  printf("%i", l); where l is a long int equals to INT_MIN - 1024
-  _printf("%i", l); where l is a long int equals to INT_MIN - 1024
+  printf("%i", l); //where l is a long int equals to INT_MIN - 1024
+  _printf("%i", l); //where l is a long int equals to INT_MIN - 1024
 
   printf("There is %i bytes in %i KB\n", 1024, 1);
   _printf("There is %i bytes in %i KB\n", 1024, 1);
